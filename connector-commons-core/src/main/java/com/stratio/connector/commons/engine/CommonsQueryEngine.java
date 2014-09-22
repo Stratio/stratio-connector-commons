d56bda1144feb31dc83bfd54a36ede2f66b63615
David Gomez <dgomezperez@gmail.com>
2014-09-22 18:22:50 +0200

    FIx Bugs2

--- connector-commons-core/src/main/java/com/stratio/connector/commons/engine/CommonsQueryEngine.java
+++ connector-commons-core/src/main/java/com/stratio/connector/commons/engine/CommonsQueryEngine.java
@@ -1,6 +1,8 @@
 package com.stratio.connector.commons.engine;
 
+import com.stratio.connector.commons.connection.Connection;
 import com.stratio.connector.commons.connection.ConnectionHandler;
+import com.stratio.connector.commons.connection.exceptions.HandlerConnectionException;
 import com.stratio.meta.common.connector.IQueryEngine;
 import com.stratio.meta.common.exceptions.ExecutionException;
 import com.stratio.meta.common.exceptions.UnsupportedException;
@@ -29,19 +31,23 @@ public abstract class CommonsQueryEngine extends CommonsUtils implements IQueryE
     }
 
 
-    public abstract QueryResult execute(ClusterName targetCluster, LogicalWorkflow workflow, ConnectionHandler  connection) throws UnsupportedException, ExecutionException ;
+    public abstract QueryResult execute(ClusterName targetCluster, LogicalWorkflow workflow,  Connection connection  ) throws UnsupportedException, ExecutionException ;
 
 
 
     @Override
     public  QueryResult execute(ClusterName targetCluster, LogicalWorkflow workflow) throws UnsupportedException, ExecutionException{
+        try{
+            QueryResult result = null;
+            startWork(targetCluster,connectionHandler);
+            result = execute(targetCluster,workflow,connectionHandler.getConnection(targetCluster.getName()));
+            endWork(targetCluster, connectionHandler);
 
-        QueryResult result = null;
-        startWork(targetCluster,connectionHandler);
-        result = execute(targetCluster,workflow,connectionHandler);
-        endWork(targetCluster, connectionHandler);
-
-        return result;
-
+            return result;
+        } catch (HandlerConnectionException e) {
+            String msg = "Error find Connection in " + targetCluster.getName() + ". " + e.getMessage();
+            logger.error(msg);
+            throw new ExecutionException(msg, e);
+        }
     }
 }
--- connector-commons-core/src/main/java/com/stratio/connector/commons/engine/CommonsStorageEngine.java
+++ connector-commons-core/src/main/java/com/stratio/connector/commons/engine/CommonsStorageEngine.java
@@ -1,6 +1,8 @@
 package com.stratio.connector.commons.engine;
 
+import com.stratio.connector.commons.connection.Connection;
 import com.stratio.connector.commons.connection.ConnectionHandler;
+import com.stratio.connector.commons.connection.exceptions.HandlerConnectionException;
 import com.stratio.meta.common.connector.IStorageEngine;
 import com.stratio.meta.common.data.Row;
 import com.stratio.meta.common.exceptions.ExecutionException;
@@ -30,27 +32,35 @@ public abstract class CommonsStorageEngine extends CommonsUtils implements IStor
         this.connectionHandler= connectionHandler;
     }
 
-    public abstract void insert(ClusterName targetCluster, TableMetadata targetTable, Row row, ConnectionHandler connectionHandler ) throws UnsupportedException, ExecutionException;
+    public abstract void insert(ClusterName targetCluster, TableMetadata targetTable, Row row,  Connection connection  ) throws UnsupportedException, ExecutionException;
 
-    public abstract void insert(ClusterName targetCluster, TableMetadata targetTable, Collection<Row> rows, ConnectionHandler connectionHandler ) throws UnsupportedException, ExecutionException;
+    public abstract void insert(ClusterName targetCluster, TableMetadata targetTable, Collection<Row> rows,  Connection connection ) throws UnsupportedException, ExecutionException;
 
 
     @Override
     public  void insert(ClusterName targetCluster, TableMetadata targetTable, Row row) throws UnsupportedException, ExecutionException{
-
-        startWork(targetCluster , connectionHandler);
-        insert(targetCluster,targetTable,row,connectionHandler);
-        endWork(targetCluster, connectionHandler);
-
+        try{
+            startWork(targetCluster , connectionHandler);
+            insert(targetCluster,targetTable,row,connectionHandler.getConnection(targetCluster.getName()));
+            endWork(targetCluster, connectionHandler);
+        } catch (HandlerConnectionException e) {
+            String msg = "Error find Connection in " + targetCluster.getName() + ". " + e.getMessage();
+            logger.error(msg);
+            throw new ExecutionException(msg, e);
+        }
     }
 
     @Override
     public  void insert(ClusterName targetCluster, TableMetadata targetTable, Collection<Row> rows) throws UnsupportedException, ExecutionException{
-
-        startWork(targetCluster , connectionHandler);
-        insert(targetCluster,targetTable,rows,connectionHandler);
-        endWork(targetCluster , connectionHandler);
-
+        try{
+            startWork(targetCluster , connectionHandler);
+            insert(targetCluster,targetTable,rows,connectionHandler.getConnection(targetCluster.getName()));
+            endWork(targetCluster , connectionHandler);
+        } catch (HandlerConnectionException e) {
+            String msg = "Error find Connection in " + targetCluster.getName() + ". " + e.getMessage();
+            logger.error(msg);
+            throw new ExecutionException(msg, e);
+        }
     }
 
 }
