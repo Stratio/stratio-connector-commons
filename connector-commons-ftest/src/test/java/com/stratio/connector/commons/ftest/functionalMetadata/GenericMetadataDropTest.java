e881f96eab257a2c1452aed5ed3a476fe3c09429
darroyostratio <darroyo@stratio.com>
2014-09-30 09:44:36 +0200

    inserting data with metadata

--- connector-commons-ftest/src/test/java/com/stratio/connector/commons/ftest/functionalMetadata/GenericMetadataDropTest.java
+++ connector-commons-ftest/src/test/java/com/stratio/connector/commons/ftest/functionalMetadata/GenericMetadataDropTest.java
@@ -29,26 +29,21 @@ import org.junit.After;
 import org.junit.Test;
 
 import com.stratio.connector.commons.ftest.GenericConnectorTest;
+import com.stratio.connector.commons.ftest.schema.TableMetadataBuilder;
 import com.stratio.connector.commons.ftest.workFlow.LogicalWorkFlowCreator;
-import com.stratio.meta.common.connector.Operations;
 import com.stratio.meta.common.data.Cell;
 import com.stratio.meta.common.data.Row;
 import com.stratio.meta.common.exceptions.ConnectionException;
 import com.stratio.meta.common.exceptions.ExecutionException;
 import com.stratio.meta.common.exceptions.UnsupportedException;
-import com.stratio.meta.common.logicalplan.LogicalStep;
 import com.stratio.meta.common.logicalplan.LogicalWorkflow;
-import com.stratio.meta.common.logicalplan.Project;
 import com.stratio.meta.common.result.QueryResult;
 import com.stratio.meta2.common.data.CatalogName;
 import com.stratio.meta2.common.data.ClusterName;
 import com.stratio.meta2.common.data.ColumnName;
-import com.stratio.meta2.common.data.IndexName;
 import com.stratio.meta2.common.data.TableName;
 import com.stratio.meta2.common.metadata.ColumnMetadata;
 import com.stratio.meta2.common.metadata.ColumnType;
-import com.stratio.meta2.common.metadata.IndexMetadata;
-import com.stratio.meta2.common.metadata.IndexType;
 import com.stratio.meta2.common.metadata.TableMetadata;
 import com.stratio.meta2.common.statements.structures.selectors.Selector;
 
@@ -67,36 +62,37 @@ public abstract class GenericMetadataDropTest extends GenericConnectorTest {
         cells.put(COLUMN_1, new Cell("value1"));
         cells.put(COLUMN_2, new Cell(2));
         row.setCells(cells);
-        if (iConnectorHelper.isTableMandatory()){
+        if (iConnectorHelper.isTableMandatory()) {
             createTable();
         }
 
-        connector.getStorageEngine().insert(clusterName,
-                new TableMetadata(new TableName(CATALOG, TABLE), null, null, null, null, Collections.EMPTY_LIST,
-                        Collections.EMPTY_LIST), row);
-        connector.getStorageEngine().insert(clusterName,
-                new TableMetadata(new TableName(OTHER_CATALOG, TABLE), null, null, null, null, Collections.EMPTY_LIST,
-                        Collections.EMPTY_LIST), row);
-        connector.getStorageEngine().insert(clusterName,
-                new TableMetadata(new TableName(CATALOG, OTHER_TABLE), null, null, null, null, Collections.EMPTY_LIST,
-                        Collections.EMPTY_LIST), row);
+        TableMetadataBuilder tableMetadataBuilder = new TableMetadataBuilder(CATALOG, TABLE);
+        tableMetadataBuilder.addColumn(COLUMN_1, ColumnType.VARCHAR).addColumn(COLUMN_2, ColumnType.INT);
+        connector.getStorageEngine().insert(clusterName, tableMetadataBuilder.build(), row);
+
+        tableMetadataBuilder = new TableMetadataBuilder(OTHER_CATALOG, TABLE);
+        tableMetadataBuilder.addColumn(COLUMN_1, ColumnType.VARCHAR).addColumn(COLUMN_2, ColumnType.INT);
+        connector.getStorageEngine().insert(clusterName, tableMetadataBuilder.build(), row);
+
+        tableMetadataBuilder = new TableMetadataBuilder(CATALOG, OTHER_TABLE);
+        tableMetadataBuilder.addColumn(COLUMN_1, ColumnType.VARCHAR).addColumn(COLUMN_2, ColumnType.INT);
+        connector.getStorageEngine().insert(clusterName, tableMetadataBuilder.build(), row);
 
         refresh(CATALOG);
         refresh(OTHER_CATALOG);
 
         connector.getMetadataEngine().dropTable(clusterName, (new TableName(CATALOG, TABLE)));
 
-        QueryResult queryResult = connector.getQueryEngine()
-                .execute( createLogicalWorkFlow(CATALOG, TABLE));
+        QueryResult queryResult = connector.getQueryEngine().execute(createLogicalWorkFlow(CATALOG, TABLE));
         assertEquals("Table [" + CATALOG + "." + TABLE + "] deleted", 0, queryResult.getResultSet().size());
 
-        queryResult = connector.getQueryEngine().execute( createLogicalWorkFlow(OTHER_CATALOG, TABLE));
+        queryResult = connector.getQueryEngine().execute(createLogicalWorkFlow(OTHER_CATALOG, TABLE));
         assertNotEquals("Table [" + OTHER_CATALOG + "." + TABLE + "] exist", 0, queryResult.getResultSet().size());
 
         queryResult = connector.getQueryEngine().execute(createLogicalWorkFlow(CATALOG, OTHER_TABLE));
         assertNotEquals("Table [" + CATALOG + "." + OTHER_TABLE + " exist", 0, queryResult.getResultSet().size());
-        if (iConnectorHelper.isTableMandatory()){
-            connector.getMetadataEngine().dropTable(getClusterName(),new TableName(CATALOG,TABLE));
+        if (iConnectorHelper.isTableMandatory()) {
+            connector.getMetadataEngine().dropTable(getClusterName(), new TableName(CATALOG, TABLE));
         }
     }
 
@@ -114,12 +110,11 @@ public abstract class GenericMetadataDropTest extends GenericConnectorTest {
         // ColumnMetadata (list of columns to create a single index)
         List<ColumnMetadata> columns = new ArrayList<>();
         Object[] parameters = null;
-        columns.add(new ColumnMetadata(new ColumnName(tableName, "columnName_1"), parameters,
-                ColumnType.TEXT));
+        columns.add(new ColumnMetadata(new ColumnName(tableName, "columnName_1"), parameters, ColumnType.TEXT));
 
         TableMetadata tableMetadata = new TableMetadata(tableName, options, columnsMap, Collections.EMPTY_MAP,
-                getClusterName(), Collections.EMPTY_LIST,Collections.EMPTY_LIST);
-        connector.getMetadataEngine().createTable(getClusterName(),tableMetadata);
+                        getClusterName(), Collections.EMPTY_LIST, Collections.EMPTY_LIST);
+        connector.getMetadataEngine().createTable(getClusterName(), tableMetadata);
     }
 
     @Test
@@ -132,23 +127,24 @@ public abstract class GenericMetadataDropTest extends GenericConnectorTest {
         cells.put(COLUMN_2, new Cell(2));
         row.setCells(cells);
 
-        connector.getStorageEngine().insert(clusterName,
-                new TableMetadata(new TableName(CATALOG, TABLE), null, null, null, null, Collections.EMPTY_LIST,
-                        Collections.EMPTY_LIST), row);
-        connector.getStorageEngine().insert(clusterName,
-                new TableMetadata(new TableName(OTHER_CATALOG, TABLE), null, null, null, null, Collections.EMPTY_LIST,
-                        Collections.EMPTY_LIST), row);
-        connector.getStorageEngine().insert(clusterName,
-                new TableMetadata(new TableName(CATALOG, OTHER_TABLE), null, null, null, null, Collections.EMPTY_LIST,
-                        Collections.EMPTY_LIST), row);
+        TableMetadataBuilder tableMetadataBuilder = new TableMetadataBuilder(CATALOG, TABLE);
+        tableMetadataBuilder.addColumn(COLUMN_1, ColumnType.VARCHAR).addColumn(COLUMN_2, ColumnType.INT);
+        connector.getStorageEngine().insert(clusterName, tableMetadataBuilder.build(), row);
+
+        tableMetadataBuilder = new TableMetadataBuilder(OTHER_CATALOG, TABLE);
+        tableMetadataBuilder.addColumn(COLUMN_1, ColumnType.VARCHAR).addColumn(COLUMN_2, ColumnType.INT);
+        connector.getStorageEngine().insert(clusterName, tableMetadataBuilder.build(), row);
+
+        tableMetadataBuilder = new TableMetadataBuilder(CATALOG, OTHER_TABLE);
+        tableMetadataBuilder.addColumn(COLUMN_1, ColumnType.VARCHAR).addColumn(COLUMN_2, ColumnType.INT);
+        connector.getStorageEngine().insert(clusterName, tableMetadataBuilder.build(), row);
 
         refresh(CATALOG);
         refresh(OTHER_CATALOG);
 
         connector.getMetadataEngine().dropCatalog(clusterName, new CatalogName(CATALOG));
 
-        QueryResult queryResult = connector.getQueryEngine()
-                .execute(createLogicalWorkFlow(CATALOG, TABLE));
+        QueryResult queryResult = connector.getQueryEngine().execute(createLogicalWorkFlow(CATALOG, TABLE));
         assertEquals("Table [" + CATALOG + "." + TABLE + "] deleted", 0, queryResult.getResultSet().size());
 
         queryResult = connector.getQueryEngine().execute(createLogicalWorkFlow(OTHER_CATALOG, TABLE));
@@ -163,9 +159,8 @@ public abstract class GenericMetadataDropTest extends GenericConnectorTest {
 
     private LogicalWorkflow createLogicalWorkFlow(String catalog, String table) {
 
-
         return new LogicalWorkFlowCreator(catalog, table, getClusterName()).addColumnName(COLUMN_1, COLUMN_2)
-                .getLogicalWorkflow();
+                        .getLogicalWorkflow();
     }
 
     @After
