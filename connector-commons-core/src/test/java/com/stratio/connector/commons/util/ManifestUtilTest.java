3c5367cff011d94f8819fb0e89bfd0913fea84eb
David <darroyo@stratio.com>
2014-11-27 12:59:52 +0100

    added apache license

--- connector-commons-core/src/test/java/com/stratio/connector/commons/util/ManifestUtilTest.java
+++ connector-commons-core/src/test/java/com/stratio/connector/commons/util/ManifestUtilTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to STRATIO (C) under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.  The STRATIO (C) licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.stratio.connector.commons.util;
 
 import static org.junit.Assert.assertEquals;
@@ -9,7 +26,9 @@ import org.junit.Test;
  *
  * @author <Authors name>
  * @version 1.0
- * @since <pre>oct 27, 2014</pre>
+ * @since <pre>
+ * oct 27, 2014
+ * </pre>
  */
 public class ManifestUtilTest {
 
@@ -17,7 +36,6 @@ public class ManifestUtilTest {
      * Method: getDatastoreName(String pathManifest)
      */
     @Test
-
     public void testGetDatastoreName() throws Exception {
         String[] datastoreName = ManifestUtil.getDatastoreName("ExampleConnector.xml");
         assertEquals("The datastore number is correct", 2, datastoreName.length);
@@ -32,8 +50,8 @@ public class ManifestUtilTest {
     @Test
     public void testGetConectorName() throws Exception {
 
-        assertEquals("The connectorName is correct", "elasticsearch", ManifestUtil.getConectorName("ExampleConnector" +
-                ".xml"));
+        assertEquals("The connectorName is correct", "elasticsearch", ManifestUtil.getConectorName("ExampleConnector"
+                        + ".xml"));
     }
 
     /**
@@ -42,4 +60,4 @@ public class ManifestUtilTest {
      *
      */
 
-} 
+}
