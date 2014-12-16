588928117463e53bab3d5ecc5d739de0c5b7c4b1
josegom <josegom@gmail.com>
2014-12-16 17:51:32 +0100

    add Project parse CON-18

---
+++ src/main/java/com/stratio/connector/commons/engine/query/ProjectParsed.java
@@ -0,0 +1,193 @@
+/*
+ * Licensed to STRATIO (C) under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.  The STRATIO (C) licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.stratio.connector.commons.engine.query;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+
+import com.stratio.crossdata.common.exceptions.UnsupportedException;
+import com.stratio.crossdata.common.logicalplan.Filter;
+import com.stratio.crossdata.common.logicalplan.GroupBy;
+import com.stratio.crossdata.common.logicalplan.Limit;
+import com.stratio.crossdata.common.logicalplan.LogicalStep;
+import com.stratio.crossdata.common.logicalplan.Project;
+import com.stratio.crossdata.common.logicalplan.Select;
+import com.stratio.crossdata.common.logicalplan.Window;
+import com.stratio.crossdata.common.statements.structures.Operator;
+
+/**
+ * This class is a representation of a ElasticSearch query.
+ * <p/>
+ * <p/>
+ * Created by jmgomez on 15/09/14.
+ */
+public class ProjectParsed {
+
+    /**
+     * Constructor.
+     * @param project the project.
+     * @throws UnsupportedException if a logical step is not supported.
+     */
+    public ProjectParsed(Project project) throws UnsupportedException {
+        this.project = project;
+        LogicalStep lStep =project;
+        while((lStep = lStep.getNextStep())!=null) {
+            addLogicalStep(lStep);
+        }
+    }
+
+    /**
+     * The project.
+     */
+    private Project project = null;
+
+    /**
+     * The filters.
+     */
+    private Collection<Filter> filterList = Collections.EMPTY_LIST;
+    /**
+     * The matchList.
+     */
+    private Collection<Filter> matchList = Collections.EMPTY_LIST;
+
+    /**
+     * The select.
+     */
+    private Select select;
+    /**
+     * The limit.
+     */
+    private Limit limit;
+    /**
+     * The groupBy.
+     */
+    private GroupBy groupBy;
+    /**
+     * The window.
+     */
+    private Window window;
+
+
+
+
+    /**
+     * Get the project.
+     *
+     * @return the project,
+     */
+    public Project getProject() {
+
+        return project;
+    }
+
+
+
+    /**
+     * Get the filter.
+     *
+     * @return the filter.
+     */
+    public Iterator<Filter> getFilter() {
+        return filterList.iterator();
+    }
+
+
+
+    /**
+     * Return The matchList.
+     *
+     * @return the matchList
+     */
+    public Iterator<Filter> getMatchList() {
+        return matchList.iterator();
+    }
+
+
+
+    /**
+     * return the select.
+     *
+     * @return the select.
+     */
+    public Select getSelect() {
+        return select;
+    }
+
+
+    /**
+     * Return the limit.
+     *
+     * @return the limit.
+     */
+    public Limit getLimit() {
+        return limit;
+    }
+
+    /**
+     * This method add the correct logical step.
+     * @param lStep logical step.
+     * @throws UnsupportedException if the logical step is not supported.
+     */
+    private void addLogicalStep(LogicalStep lStep) throws UnsupportedException {
+        if (lStep instanceof Project){
+            project =(Project)lStep;
+        }else if (lStep instanceof Filter) {
+
+            decideTypeFilterToAdd((Filter) lStep);
+
+        }else if (lStep instanceof Select) {
+            select=(Select)lStep;
+
+        } else if (lStep instanceof Limit) {
+            limit = (Limit)lStep;
+
+        }else if (lStep instanceof GroupBy) {
+            groupBy= (GroupBy) lStep;
+
+        }else if (lStep instanceof Window){
+            window=(Window) lStep;
+
+        }else {
+            throw new UnsupportedException(
+                    "LogicalStep [" + lStep.getClass().getCanonicalName() + " not supported");
+        }
+    }
+
+    /**
+     * Add filter in the correct list.
+     * @param filter the filter.
+     */
+    private void decideTypeFilterToAdd(Filter filter) {
+        Filter step = filter;
+
+        if (Operator.MATCH == step.getRelation().getOperator()) {
+            if (matchList.isEmpty()){
+                matchList = new ArrayList<>();
+            }
+            matchList.add(filter);
+        } else {
+            if (filterList.isEmpty()){
+                filterList = new ArrayList<>();
+            }
+            filterList.add(filter);
+        }
+    }
+}
---
+++ src/test/java/com/stratio/connector/commons/engine/query/ProjectParsedTest.java
@@ -0,0 +1,140 @@
+package com.stratio.connector.commons.engine.query;
+
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Iterator;
+
+import org.junit.Test;
+
+import com.stratio.crossdata.common.data.ClusterName;
+import com.stratio.crossdata.common.data.TableName;
+import com.stratio.crossdata.common.logicalplan.Filter;
+import com.stratio.crossdata.common.logicalplan.Limit;
+import com.stratio.crossdata.common.logicalplan.Project;
+import com.stratio.crossdata.common.logicalplan.Select;
+import com.stratio.crossdata.common.metadata.Operations;
+import com.stratio.crossdata.common.statements.structures.Operator;
+import com.stratio.crossdata.common.statements.structures.Relation;
+
+/** 
+* ProjectParsed Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>dic 16, 2014</pre> 
+* @version 1.0 
+*/ 
+public class ProjectParsedTest {
+
+   private static final String CATALOG = "catalog";
+   private static final String TABLE = "table";
+   private static final TableName TABLE_NAME = new TableName(CATALOG, TABLE);
+   private static final String CLUSTER = "cluster";
+   private static final ClusterName CLUSTER_NAME = new ClusterName(CLUSTER);
+
+   /**
+* 
+* Method: hasProjection() 
+* 
+*/ 
+@Test
+public void testCreateProjecParsedProject() throws Exception {
+   Project project = new Project(Operations.PROJECT,TABLE_NAME,CLUSTER_NAME);
+
+
+   ProjectParsed projectParsed = new ProjectParsed(project);
+   assertFalse("The filter list must be empty", projectParsed.getFilter().hasNext());
+   assertNull("The limit must be null", projectParsed.getLimit());
+   assertFalse("The filter match must be empty", projectParsed.getFilter().hasNext());
+   assertNull("The select must be null", projectParsed.getSelect());
+   assertEquals("The project must be the project pass in constructor", project, projectParsed.getProject());
+
+}
+
+
+
+   @Test
+   public void testCreateProjecParsedProjectSelect() throws Exception {
+      Project project = new Project(Operations.PROJECT,TABLE_NAME,CLUSTER_NAME);
+      Select select = new Select(Operations.SELECT_OPERATOR,null,null,null);
+      project.setNextStep(select);
+
+      ProjectParsed projectParsed = new ProjectParsed(project);
+      assertFalse("The filter list must be empty", projectParsed.getFilter().hasNext());
+      assertNull("The limit must be null", projectParsed.getLimit());
+      assertFalse("The filter match list be empty", projectParsed.getMatchList().hasNext());
+      assertEquals("The select must be the select created before",select, projectParsed.getSelect());
+      assertEquals("The project must be the project pass in constructor",project,projectParsed.getProject());
+
+   }
+
+
+
+   @Test
+   public void testCreateProjecParsedProjecFilter() throws Exception {
+      Project project = new Project(Operations.PROJECT,TABLE_NAME,CLUSTER_NAME);
+
+      Relation relation = new Relation(null, Operator.EQ,null);
+      Filter filter = new Filter(Operations.FILTER_INDEXED_EQ,relation);
+      project.setNextStep(filter);
+
+      ProjectParsed projectParsed = new ProjectParsed(project);
+
+      Iterator<Filter> filterIterator = projectParsed.getFilter();
+      assertTrue("The filter list must one element", filterIterator.hasNext());
+      assertEquals("The filter element must be the filter created before", filter, filterIterator.next());
+      assertFalse("The filter list must only one element", filterIterator.hasNext());
+
+      assertNull("The limit must be null", projectParsed.getLimit());
+      assertFalse("The filter match list be empty", projectParsed.getMatchList().hasNext());
+      assertNull("The select must be null", projectParsed.getSelect());
+      assertEquals("The project must be the project pass in constructor",project,projectParsed.getProject());
+
+   }
+
+
+
+   @Test
+   public void testCreateProjecParsedProjecMatch() throws Exception {
+      Project project = new Project(Operations.PROJECT,TABLE_NAME,CLUSTER_NAME);
+
+      Relation relation = new Relation(null, Operator.MATCH,null);
+      Filter filter = new Filter(Operations.FILTER_INDEXED_MATCH,relation);
+      project.setNextStep(filter);
+
+      ProjectParsed projectParsed = new ProjectParsed(project);
+
+      assertFalse("The filter list must be empty", projectParsed.getFilter().hasNext());
+
+      Iterator<Filter> matchIterator = projectParsed.getMatchList();
+      assertTrue("The filter match list must one element", matchIterator.hasNext());
+      assertEquals("The filter match  element must be the filter created before", filter, matchIterator
+              .next());
+      assertFalse("The filter match must only one element", matchIterator.hasNext());
+      assertNull("The limit must be null", projectParsed.getLimit());
+
+      assertNull("The select must be null", projectParsed.getSelect());
+      assertEquals("The project must be the project pass in constructor",project,projectParsed.getProject());
+
+   }
+
+
+   @Test
+   public void testCreateProjecParsedProjectLimit() throws Exception {
+      Project project = new Project(Operations.PROJECT,TABLE_NAME,CLUSTER_NAME);
+      Limit limit = new Limit(Operations.SELECT_LIMIT,10);
+      project.setNextStep(limit);
+
+      ProjectParsed projectParsed = new ProjectParsed(project);
+      assertFalse("The filter list must be empty", projectParsed.getFilter().hasNext());
+      assertEquals("The limit must be the limit created before", limit,projectParsed.getLimit());
+      assertFalse("The filter match list be empty", projectParsed.getMatchList().hasNext());
+      assertNull("The select must be null", projectParsed.getSelect());
+      assertEquals("The project must be the project pass in constructor",project,projectParsed.getProject());
+
+   }
+
+
+} 
