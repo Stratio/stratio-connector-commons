9842969abeadeb1b78eff46cf741bcfc76148bc5
David <darroyo@stratio.com>
2015-01-29 16:05:51 +0100

    added new unit test for CatalogMetadataBuilder

--- src/test/java/com/stratio/connector/commons/metadata/CatalogMetadataBuilderTest.java
+++ src/test/java/com/stratio/connector/commons/metadata/CatalogMetadataBuilderTest.java
@@ -17,6 +17,64 @@
  */
 package com.stratio.connector.commons.metadata;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.stratio.crossdata.common.data.TableName;
+import com.stratio.crossdata.common.metadata.CatalogMetadata;
+import com.stratio.crossdata.common.metadata.TableMetadata;
+import com.stratio.crossdata.common.statements.structures.Selector;
+import com.stratio.crossdata.common.statements.structures.StringSelector;
+
 public class CatalogMetadataBuilderTest {
+
+    private static final String CATALOG = "catalog";
+
+    @Test
+    public void basicCatalogTest() {
+
+        CatalogMetadataBuilder catalogMetadataBuilder = new CatalogMetadataBuilder(CATALOG);
+
+        CatalogMetadata catalogMetadata = catalogMetadataBuilder.build();
+
+        Assert.assertEquals("Catalog name is not the expected", CATALOG, catalogMetadata.getName().getName());
+        Assert.assertTrue("The catalog should not have tables", catalogMetadata.getTables().isEmpty());
+
+    }
+
+    @Test
+    public void catalogWithOptionsTest() {
+
+        CatalogMetadataBuilder catalogMetadataBuilder = new CatalogMetadataBuilder(CATALOG);
+        Selector selector = new StringSelector("key");
+        Selector selectorValue = new StringSelector("value");
+        Map<Selector, Selector> options = new HashMap<Selector, Selector>();
+        options.put(selector, selectorValue);
+
+        CatalogMetadata catalogMetadata = catalogMetadataBuilder.withOptions(options).build();
+
+        Assert.assertEquals("Catalog name is not the expected", CATALOG, catalogMetadata.getName().getName());
+        Assert.assertEquals("The options are not the expected", selectorValue,
+                        catalogMetadata.getOptions().get(selector));
+    }
+
+    @Test
+    public void catalogWithTableMetadataTest() {
+
+        CatalogMetadataBuilder catalogMetadataBuilder = new CatalogMetadataBuilder(CATALOG);
+        TableName tableName = new TableName(CATALOG, "table");
+        TableMetadata tableMetadata = new TableMetadata(tableName, null, null, null, null, null, null);
+
+        CatalogMetadata catalogMetadata = catalogMetadataBuilder.withTables(tableMetadata).build();
+
+        Assert.assertEquals("Catalog name is not the expected", CATALOG, catalogMetadata.getName().getName());
+        Assert.assertEquals("There should be 1 table", 1, catalogMetadata.getTables().size());
+        Assert.assertTrue("The table " + tableName.getQualifiedName() + " has not been found", catalogMetadata
+                        .getTables().containsKey(tableName));
+
+    }
 
 }
