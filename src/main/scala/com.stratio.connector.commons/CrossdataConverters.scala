ab10154e79a1316cb260e40d6c4d69975e6839fe
Javier Santos <javiersantospaniego@gmail.com>
2015-03-12 17:15:10 +0100

    + Added SparkSQL->Crossdata rows converters.

---
+++ src/main/scala/com.stratio.connector.commons/CrossdataConverters.scala
@@ -0,0 +1,107 @@
+/*
+ * Licensed to STRATIO (C) under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.  The STRATIO (C) licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.stratio.connector.commons
+
+import scala.language.implicitConversions
+import scala.collection.mutable.ArrayBuffer
+import org.apache.spark.sql.catalyst.expressions.GenericRow
+import org.apache.spark.sql.catalyst.types.{ArrayType, DataType, StructType}
+import org.apache.spark.sql.{Row => SparkSQLRow, SchemaRDD}
+import com.stratio.crossdata.common.metadata.{ColumnMetadata, ColumnType}
+import com.stratio.crossdata.common.data.{Row => XDRow, Cell, ResultSet}
+
+object CrossdataConverters {
+
+  import scala.collection.JavaConversions._
+
+  type ColumnTypeMap = Map[String, ColumnType]
+
+  /**
+   * Compute some SchemaRDD and map it into a Crossdata ResultSet
+   * @param schemaRDD Given Schema RDD
+   * @param metadata Columns metadata
+   * @return An equivalent ResultSet
+   */
+  def toResultSet(
+    schemaRDD: SchemaRDD,
+    metadata: List[ColumnMetadata]): ResultSet =
+    toResultSet(schemaRDD.toLocalIterator, schemaRDD.schema, metadata)
+
+  /**
+   * Compute some SchemaRDD and map it into a Crossdata ResultSet
+   * @param rows Iterator of Crossdata rows to be converted
+   * @param schema Row structure
+   * @param metadata Columns metadata
+   * @return A new Resultset with all converted rows
+   */
+  def toResultSet(
+    rows: Iterator[SparkSQLRow],
+    schema: StructType,
+    metadata: List[ColumnMetadata]): ResultSet = {
+    val resultSet = new ResultSet
+    resultSet.setColumnMetadata(metadata)
+    rows.foreach(row => resultSet.add(toCrossDataRow(row, schema)))
+    resultSet
+  }
+
+  /**
+   * Convert some SparkSQL row into a Crossdata row
+   * @param row SparkSQL row to be converted
+   * @param schema Row structure
+   * @return A new Resultset with all converted rows
+   */
+  def toCrossDataRow(row: SparkSQLRow, schema: StructType): XDRow = {
+    val fields = schema.fields
+    val xdRow = new XDRow()
+    fields.zipWithIndex.map {
+      case (field, idx) => xdRow.addCell(
+        field.name,
+        new Cell(toCellValue(row(idx), field.dataType)))
+    }
+    xdRow
+  }
+
+  /**
+   * Convert some undefined value into Crossdata cell
+   * @param value Given value to be converted to cell
+   * @param dataType Object data type.
+   * @return A new Crossdata cell with converted object.
+   */
+  def toCellValue(value: Any, dataType: DataType): Any = {
+    import scala.collection.JavaConversions._
+    Option(value).map { v =>
+      (dataType, value) match {
+        case (ArrayType(elementType, _), array: ArrayBuffer[Any@unchecked]) =>
+          val list: java.util.List[Any] = array.map {
+            case obj => toCellValue(obj, elementType)
+          }.toList
+          list
+        case (struct: StructType, value: GenericRow) =>
+          val map: java.util.Map[String, Any] = struct.fields.zipWithIndex.map {
+            case (field, idx) =>
+              field.name -> toCellValue(value(idx), field.dataType)
+          }.toMap[String, Any]
+          map
+        case _ =>
+          value
+      }
+    }.orNull
+  }
+
+}
--- src/main/scala/com.stratio.connector.commons/timer.scala
+++ src/main/scala/com.stratio.connector.commons/timer.scala
@@ -1,9 +1,37 @@
+/*
+ * Licensed to STRATIO (C) under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.  The STRATIO (C) licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
 package com.stratio.connector.commons
 
 import org.slf4j.Logger
 
 object timer {
 
+  /**
+   * Evals some {{{T}}} by-name expression and get times spent on it.
+   *
+   * @param op Description of what {{{f}}} does
+   * @param f By-name {{{T}}} expression
+   * @param logger Implicit logger used to print out elapsed time
+   * @tparam T Expression type
+   * @return Expression value
+   */
   def time[T](op: String)(f: => T)(implicit logger: Logger): T = {
     val before = System.currentTimeMillis()
     val t = f
