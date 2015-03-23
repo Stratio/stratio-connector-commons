4439572c26f54ee5332ed5511e476f42e805b175
Jose Manuel Gomez <jmgomez@stratio.com>
2015-03-23 15:44:12 +0100

    ContinuousDelivery

--- doc/src/site/sphinx/conf.py
+++ doc/src/site/sphinx/conf.py
@@ -109,6 +109,7 @@ html_theme = 'stratio'
 html_theme_options = {
 'versions' : ['0.5.0-SNAPSHOT'],
 'github' : 'https://github.com/Stratio/stratio-connector-commons.git',
+'jira' : 'https://github.com/Stratio/stratio-connector-commons/issues',
 'module_name' : 'connector-commons'
 }
 
