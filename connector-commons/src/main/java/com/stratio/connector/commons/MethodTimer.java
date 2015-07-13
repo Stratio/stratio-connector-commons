71e572743bbc9ea4ff073a70110dd56ed1ac33ed
ccaballe <criswol90@gmail.com>
2015-07-13 14:21:49 +0200

    Finished timer implementation

--- connector-commons/src/main/java/com/stratio/connector/commons/MethodTimer.java
+++ connector-commons/src/main/java/com/stratio/connector/commons/MethodTimer.java
@@ -1,23 +1,41 @@
 package com.stratio.connector.commons;
 
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
+import com.stratio.crossdata.common.utils.Metrics;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by ccaballero on 7/9/15.
  */
 @Aspect
 public class MethodTimer {
+    private final transient Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Around("execution(* *(..)) && @annotation(TimerJ)")
     public Object around(ProceedingJoinPoint point) throws Throwable {
-        long start = System.nanoTime();
-        Object result = point.proceed();
-        long finish = System.nanoTime() - start;
         String methodName = MethodSignature.class.cast(point.getSignature()).getMethod().getName();
         String methodArgs = point.getArgs().toString();
-        System.out.println("Method "+methodName+" with args "+ methodArgs +" executed in "+finish+ " ns");
+        MetricRegistry metricRegistry = Metrics.getRegistry();
+        Timer timer;
+        synchronized (this) {
+            if(metricRegistry.getTimers().containsKey(methodName)){
+                timer = metricRegistry.getTimers().get(methodName);
+            }
+            else{
+                timer = metricRegistry.register(methodName, new Timer());
+            }
+        }
+        Timer.Context before = timer.time();
+        Object result = point.proceed();
+        long timeAfter = before.stop();
+
+        logger.debug("Method "+methodName+" with args "+ methodArgs +" executed in "+timeAfter+ " ns");
         return result;
     }
 }
